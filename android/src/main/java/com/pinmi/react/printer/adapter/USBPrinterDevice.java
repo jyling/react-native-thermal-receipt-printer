d040fabd6080bd680e630fc8b615d0715e84caf0
jyling <samuel@lavax.co>
2023-10-28 21:03:34 +0800

    Update USBPrinterDevice.java

--- android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterDevice.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterDevice.java
@@ -31,6 +31,7 @@ public class USBPrinterDevice implements PrinterDevice{
     @Override
     public WritableMap toRNWritableMap() {
         WritableMap deviceMap = Arguments.createMap();
+        deviceMap.putString("device_name", this.mDevice.getDeviceName());
         deviceMap.putInt("device_id", this.mDevice.getDeviceId());
         deviceMap.putInt("vendor_id", this.mDevice.getVendorId());
         deviceMap.putInt("product_id", this.mDevice.getProductId());
